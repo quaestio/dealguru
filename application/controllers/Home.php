5765425cc009fb3984d0693b273edcf86ca04701
Suman Pal <mail.dealsguru@gmail.com>
2016-01-17 21:26:02 +0530

    Adding the home page controller and home view

---
+++ application/controllers/Home.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	function __construct()
+	{
+		// Call the Model constructor
+		parent::__construct();
+		$this->load->model('login_model');
+	}
+	public function index()
+	{
+		$data['msg']="this is msg";
+		$data['deals']=$this->login_model->select_deals();
+		$this->load->view('home',$data);
+	}
+
+	
+}
